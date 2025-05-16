b6837550ca81a74e96c1e359c572a19620b3f8fa
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-05-16 10:58:13 -0400

    Week2 - complete

---
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using DotNetBookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBookstore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // use the Category model to generate 10 categories in memory for display in the view
+            var categories = new List<Category>();
+            for (int i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i });
+            }
+            return View(categories);
+        }
+
+        public IActionResult Browse(string category) 
+        {
+            // display the selected category using ViewBag object
+            if (category == null)
+            {
+                               return RedirectToAction("Index");
+            }
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
