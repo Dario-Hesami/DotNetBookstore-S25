e1ea88393741ec0d9419f3ea405a2edfe66fe725
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-06-13 09:00:56 -0400

    Week 6 - home page

--- DotNetBookstore/Controllers/HomeController.cs
+++ DotNetBookstore/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
+using DotNetBookstore.Data;
 using DotNetBookstore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetBookstore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // Hardcoded list of featured books
+            var featuredBookIds = new[] { 2, 3, 4 };
+            var featuredBooks = _context.Books
+                .Where(b => featuredBookIds.Contains(b.BookId))
+                .OrderBy(b => b.Author)
+                .ThenBy(b => b.Title);              
+            return View(await featuredBooks.ToListAsync());
         }
 
         public IActionResult Privacy()
